68502214279cc1319b9cc766592813e4e8967e75
y-l-mochizuki <mczk9402@gmail.com>
2024-12-14 23:02:30 +0900

    revert

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
 	darkMode: ["class"],
-	content: [],
+	content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
 			fontFamily: {
